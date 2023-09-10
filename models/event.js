4894e07e43c186d93f983b416ee5cdf4b86532dc
Craig McNaughton <therealcraigmcn@gmail.com>
2023-09-10 13:12:36 -0700

    Update Event model
    - allow optional ID
    - generate sequential, numeric ID

--- models/event.js
+++ models/event.js
@@ -1,40 +1,47 @@
 import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
-
-  id : {
-    type : Number,
-    unique : true,
-    required : true,
-    autoIncrement: true,
-    index : true
+  id: {
+    type: Number,
+    unique: true,
+    index: true
   },
-  type : {
-    type : String,
-    required : true
+  type: {
+    type: String,
+    required: true,
+    enum: ["PushEvent", "ReleaseEvent", "WatchEvent"]
   },
 
-  public : {
-    type : Boolean,
-    required : true
+  public: {
+    type: Boolean,
+    required: true
   },
 
-  repo_id : {
-    type : Number,
-    required : true
+  repo_id: {
+    type: Number,
+    required: true
   },
 
-  actor_id : {
-    type : Number,
-    required : true
-  },
-}, {timestamps: false, versionKey: false});
+  actor_id: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: false, versionKey: false });
 
-eventSchema.set('toJSON', {
+eventSchema.pre("save", async function (next) {
+  const doc = this;
+  if (doc.isNew) {
+    const maxSequence = await Event.find({}).sort({ id: -1 }).limit(1).then(events => events[0]?.id);
+    doc.id = maxSequence ? maxSequence + 1 : 1;
+  }
+  next();
+});
+
+eventSchema.set("toJSON", {
   virtuals: true,
   transform: (doc, ret, options) => {
-      delete ret._id;
-  },
+    delete ret._id;
+  }
 });
 
 const Event = mongoose.model("Event", eventSchema);
