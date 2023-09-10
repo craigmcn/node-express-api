d5d349ed8e96726ca9cb6faeb1aaddc247333cbc
Craig McNaughton <therealcraigmcn@gmail.com>
2023-09-10 13:12:53 -0700

    Add routes

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
-import { createEvent,getAllEvents,getEventByRepoId,getEventByEventId,getEventsByUserId } from "../controllers/eventController.js";
+import {
+  createEvent,
+  getAllEvents,
+  getEventsByRepoId,
+  getEventByEventId,
+  getEventsByUserId
+} from "../controllers/eventController.js";
+
 const EventRouter = Router();
 
-
+EventRouter.get("/events/", getAllEvents);
+EventRouter.post("/events/", createEvent);
+EventRouter.get("/repos/:repo_id/events/", getEventsByRepoId);
+EventRouter.get("/users/:user_id/events/", getEventsByUserId);
+EventRouter.get("/events/:event_id/", getEventByEventId);
 
 export { EventRouter };
